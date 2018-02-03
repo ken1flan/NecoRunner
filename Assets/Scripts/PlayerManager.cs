3c7b5f16b824f977b3427e35fc4f70d58cd5a411
ken1flan <ken1flan@gmail.com>
2018-02-03 15:53:48 +0900

    ジャンプ処理の追加

--- Assets/Scripts/PlayerManager.cs
+++ Assets/Scripts/PlayerManager.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class PlayerManager : MonoBehaviour {
+	public LayerMask blockLayer;	// ブロックレイヤ
 	private Rigidbody2D rbody;		// プレイヤー制御用Ridgebody2D
 
 	private const float MOVE_SPEED = 3;	// スピード
@@ -13,6 +14,9 @@ public class PlayerManager : MonoBehaviour {
 		RIGHT,
 	};
 	private MOVE_DIR moveDirection = MOVE_DIR.STOP;	// 移動方向
+	private float jumpPower = 300;			// ジャンプ力
+	private bool goJump = false;			// ジャンプしたか否か
+	private bool canJump = false;			// ジャンプが可能か
 
 	// Use this for initialization
 	void Start () {
@@ -21,6 +25,8 @@ public class PlayerManager : MonoBehaviour {
 
 	// Update is called once per frame
 	void Update () {
+		canJump = Physics2D.Linecast (transform.position - (transform.right * 0.2f), transform.position - (transform.up * 0.1f), blockLayer)
+			|| Physics2D.Linecast (transform.position + (transform.right * 0.2f), transform.position - (transform.up * 0.1f), blockLayer);
 
 	}
 
@@ -41,6 +47,12 @@ public class PlayerManager : MonoBehaviour {
 		}
 
 		rbody.velocity = new Vector2 (moveSpeed, rbody.velocity.y);
+
+		// ジャンプ処理
+		if (goJump) {
+			rbody.AddForce (Vector2.up * jumpPower);
+			goJump = false;
+		}
 	}
 
 	public void PushLeftButton () {
@@ -53,5 +65,12 @@ public class PlayerManager : MonoBehaviour {
 
 	public void ReleaseMoveButton () {
 		moveDirection = MOVE_DIR.STOP;
+	}
+
+	public void PushJumpButton () {
+		Debug.Log ("canJump:" + canJump);
+		if (canJump) {
+			goJump = true;
+		}
 	}
 }
