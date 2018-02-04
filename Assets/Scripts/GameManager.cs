5daaaa4fa39ee39c4538e02ffea4ae8c42e9cd02
ken1flan <ken1flan@gmail.com>
2018-02-05 07:12:16 +0900

    ゲームオーバーの処理を追加

---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour {
+	public GameObject textGameOver;	// ゲームオーバーテキスト
+	public GameObject buttons;		// 操作ボタン
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+	// ゲームオーバー処理
+	public void GameOver () {
+		textGameOver.SetActive (true);
+		buttons.SetActive (false);
+	}
+}
