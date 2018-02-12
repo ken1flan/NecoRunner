3f0fa1d6af91cb7b6e2bb8b3c0b053c18c530ed3
ken1flan <ken1flan@gmail.com>
2018-02-12 11:23:01 +0900

    ベストタイムを保存

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -8,6 +8,7 @@ public class GameManager : MonoBehaviour {
 	public GameObject textGameClear;	// ゲームクリアテキスト
 	public GameObject buttons;			// 操作ボタン
 	public GameObject textTime;			// タイム表示
+	public GameObject textBestTime;		// ベストタイム
 
 	private float time = 0;			// 現在の経過時間
 	public enum STATUS {
@@ -17,8 +18,16 @@ public class GameManager : MonoBehaviour {
 	};
 	private STATUS status = STATUS.PLAYING;
 
+	// 保存用キー
+	private const string SAVE_KEY_BEST_TIME = "SAVE_KEY_BEST_TIME";	// ベストタイム
+	private const float BEST_TIME_DEFAULT = 9999.99f;	// 初期ベストタイム
+
 	// Use this for initialization
 	void Start () {
+		// ベストタイム読み込み
+		var bestTime = PlayerPrefs.GetFloat (SAVE_KEY_BEST_TIME, BEST_TIME_DEFAULT);
+		textBestTime.GetComponent<Text> ().text = bestTime.ToString ("Best ###0.00 Sec");
+
 	}
 	
 	// Update is called once per frame
@@ -45,5 +54,19 @@ public class GameManager : MonoBehaviour {
 		textGameClear.SetActive (true);
 		buttons.SetActive (false);
 		status = STATUS.GAMECLEAR;
+
+		// ベストタイム更新
+		UpdateBestTime ();
+	}
+
+	// ベストタイム更新
+	void UpdateBestTime () {
+		var bestTime = PlayerPrefs.GetFloat (SAVE_KEY_BEST_TIME, BEST_TIME_DEFAULT);
+		if (time < bestTime) {
+			PlayerPrefs.SetFloat (SAVE_KEY_BEST_TIME, time);
+
+			string newTimeString = time.ToString ("Best ###0.00 Sec");
+			textBestTime.GetComponent<Text> ().text = newTimeString;
+		}
 	}
 }
