60c1afae41b7d756aa1d9be437523dad54619ef7
ken1flan <ken1flan@gmail.com>
2018-02-13 22:04:40 +0900

    タイトルを表示するようにした

--- Assets/Scripts/Title/TitleManager.cs
+++ Assets/Scripts/Title/TitleManager.cs
@@ -1,16 +1,22 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class TitleManager : MonoBehaviour {
 
 	// Use this for initialization
 	void Start () {
-		
+
 	}
 	
 	// Update is called once per frame
 	void Update () {
 		
+	}
+
+	// ゲーム開始
+	public void StartGame () {
+		SceneManager.LoadScene ("GameScene1");
 	}
 }
