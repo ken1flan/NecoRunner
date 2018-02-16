510d30043371553898540288498efbdef05722bf
ken1flan <ken1flan@gmail.com>
2018-02-17 00:06:04 +0900

    タイトルのスタートボタンのSEを追加

--- Assets/Scripts/Title/TitleManager.cs
+++ Assets/Scripts/Title/TitleManager.cs
@@ -4,10 +4,12 @@ using UnityEngine;
 using UnityEngine.SceneManagement;
 
 public class TitleManager : MonoBehaviour {
+	private AudioSource audioSource;	// オーディオソース
+	public AudioClip startSe;			// スタートボタン押し下げ音
 
 	// Use this for initialization
 	void Start () {
-
+		audioSource = this.gameObject.GetComponent<AudioSource> ();
 	}
 	
 	// Update is called once per frame
@@ -17,6 +19,18 @@ public class TitleManager : MonoBehaviour {
 
 	// ゲーム開始
 	public void StartGame () {
+		// BGMを止める
+		audioSource.Stop();
+
+		// ピロンと音を鳴らす
+		audioSource.PlayOneShot(startSe);
+
+		// なり終わるのを待って、ゲームにシーン変更
+		Invoke("ChangeToGameScene", 2.0f);
+	}
+
+	// ゲームシーン切り替え
+	void ChangeToGameScene () {
 		SceneManager.LoadScene ("GameScene1");
 	}
 }
