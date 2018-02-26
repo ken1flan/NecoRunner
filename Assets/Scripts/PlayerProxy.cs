dcdb640031456a585066469b66b758565987e0f9
ken1flan <ken1flan@gmail.com>
2018-02-26 21:29:52 +0900

    CanvasUIの各オブジェクトからPlayerと関連を持っていたので関連が切れやすい
    Playerへのメッセージ送信をPlayerProxy経由に変えた

---
+++ Assets/Scripts/PlayerProxy.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerProxy : MonoBehaviour {
+	public GameObject player;
+	PlayerManager playerManager;
+
+	// Use this for initialization
+	void Start () {
+		playerManager = player.GetComponent<PlayerManager> ();
+	}
+
+	// Update is called once per frame
+	void Update () {
+
+	}
+
+	public void PushLeftButton () {
+		playerManager.PushLeftButton ();
+	}
+
+	public void PushRightButton () {
+		playerManager.PushRightButton ();
+	}
+
+	public void ReleaseMoveButton () {
+		playerManager.ReleaseMoveButton ();
+	}
+
+	public void PushJumpButton () {
+		playerManager.PushJumpButton ();
+	}
+}
