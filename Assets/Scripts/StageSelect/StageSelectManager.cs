4c6ccce9f0c8f2ce894b2f1493f61a615a44b9ea
ken1flan <ken1flan@gmail.com>
2018-03-01 00:32:42 +0900

    BGM/SEの追加

--- Assets/Scripts/StageSelect/StageSelectManager.cs
+++ Assets/Scripts/StageSelect/StageSelectManager.cs
@@ -4,10 +4,14 @@ using UnityEngine;
 using UnityEngine.SceneManagement;
 
 public class StageSelectManager : MonoBehaviour {
+	private AudioSource audioSource;
+	public AudioClip startSe;
+	private string sceneName;
 
 	// Use this for initialization
 	void Start () {
-
+		// audioSource = this.gameObject.GetComponent<AudioSource>();
+		audioSource = this.gameObject.GetComponent<AudioSource> ();
 	}
 
 	// Update is called once per frame
@@ -17,11 +21,20 @@ public class StageSelectManager : MonoBehaviour {
 
 	// Stage 1 が押された
 	public void PushStage1 () {
-		SceneManager.LoadScene ("GameScene1");
+		sceneName = "GameScene1";
+		StartCoroutine (ChangeScene ());
 	}
 
 	// Stage 2 が押された
 	public void PushStage2 () {
-		SceneManager.LoadScene ("Stage1Scene");
+		sceneName = "Stage1Scene";
+		StartCoroutine (ChangeScene ());
+	}
+
+	IEnumerator ChangeScene () {
+		audioSource.Stop();
+		audioSource.PlayOneShot(startSe);
+		yield return new WaitForSeconds(0.5f);
+		SceneManager.LoadScene (sceneName);
 	}
 }
