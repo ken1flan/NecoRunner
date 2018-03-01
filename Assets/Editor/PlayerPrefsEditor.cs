32e658b911dadb12d93483c1634b00a693585dc3
ken1flan <ken1flan@gmail.com>
2018-03-02 07:35:45 +0900

    Delete player prefs

---
+++ Assets/Editor/PlayerPrefsEditor.cs
@@ -0,0 +1,11 @@
+﻿using UnityEditor;
+using UnityEngine;
+
+public class PlayerPrefsEditor {
+	[MenuItem("Tools/PlayerPrefs/DeletAll")]
+	static void DeleteAll () {
+		PlayerPrefs.DeleteAll ();
+		Debug.Log("Delete All Data of PlayerPrefs.");
+	}
+
+}
