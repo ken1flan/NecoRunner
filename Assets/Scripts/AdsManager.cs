c4963d9ebc36b0b556847d5646e074166c186e89
ken1flan <ken1flan@gmail.com>
2018-03-03 16:41:49 +0900

    ボタンを押すと広告動画を流れるようにした

---
+++ Assets/Scripts/AdsManager.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.Advertisements;
+
+public class AdsManager : MonoBehaviour {
+
+	#if UNITY_IOS
+	private string gameId = "1721184";
+	#elif UNITY_ANDROID
+	private string gameId = "1721183";
+	#endif
+
+	// Use this for initialization
+	void Start () {
+		Advertisement.Initialize(gameId);
+		GetComponent<Button> ().onClick.AddListener(OnStartAds);
+	}
+
+	// Update is called once per frame
+	void Update () {
+	}
+
+	public void OnStartAds () {
+		Advertisement.Show();
+	}
+}
