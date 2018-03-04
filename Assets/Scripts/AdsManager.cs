c11fec8b2ac925a2592e1677a36d34b41a346d4a
ken1flan <ken1flan@gmail.com>
2018-03-04 09:39:47 +0900

    広告ボタンを右から左に動くようにした

--- Assets/Scripts/AdsManager.cs
+++ Assets/Scripts/AdsManager.cs
@@ -12,14 +12,27 @@ public class AdsManager : MonoBehaviour {
 	private string gameId = "1721183";
 	#endif
 
+	private Rigidbody2D rbody;
+	private const float SPEED = -50f;
+	private float currentSpeed = SPEED;
+	private const float LEFT_SIDE = -160f;
+	private const float RIGHT_SIDE = 1440f;
+
 	// Use this for initialization
 	void Start () {
 		Advertisement.Initialize(gameId);
 		GetComponent<Button> ().onClick.AddListener(OnStartAds);
+
+		rbody = GetComponent<Rigidbody2D> ();
+		rbody.velocity = new Vector2 (SPEED, 0);
 	}
 
 	// Update is called once per frame
 	void Update () {
+		// 左端まで行ってたら右端から出てくる
+		if (transform.position.x < LEFT_SIDE) {
+			transform.position = new Vector2 (RIGHT_SIDE, transform.position.y);
+		}
 	}
 
 	public void OnStartAds () {
