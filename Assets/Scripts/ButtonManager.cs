bb742d0743d794dc14855d4522a66e7a38b87581
ken1flan <ken1flan@gmail.com>
2018-07-16 10:11:20 +0900

    ボタンのイベントリスナの設定をスクリプト内で行うようにした
    もともとPlayerProxy経由で呼び出す関数を設定していたが、
    PlayerProxyを設定するのを忘れることも多々あるので、
    スクリプト内で直接設定するように変える。

--- Assets/Scripts/ButtonManager.cs
+++ Assets/Scripts/ButtonManager.cs
@@ -1,16 +1,59 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.EventSystems;
 
 public class ButtonManager : MonoBehaviour {
+	private PlayerManager playerManager;
 
 	// Use this for initialization
 	void Start () {
-		
+		playerManager = GameObject.Find("Player").GetComponent<PlayerManager> ();
+
+		SetLeftButton ();
+		SetRightButton ();
+		SetJumpButton ();
 	}
-	
+
 	// Update is called once per frame
 	void Update () {
-		
+		// Do nothing
+	}
+
+	private void SetLeftButton () {
+		EventTrigger eventTrigger = transform.Find("ButtonLeft").GetComponent<EventTrigger> ();
+
+		EventTrigger.Entry pushButtonEntry = new EventTrigger.Entry();
+		pushButtonEntry.eventID = EventTriggerType.PointerDown;
+		pushButtonEntry.callback.AddListener((data) => { playerManager.PushLeftButton(); });
+		eventTrigger.triggers.Add(pushButtonEntry);
+
+		EventTrigger.Entry releaseButtonEntry = new EventTrigger.Entry();
+		releaseButtonEntry.eventID = EventTriggerType.PointerUp;
+		releaseButtonEntry.callback.AddListener((data) => { playerManager.ReleaseMoveButton(); });
+		eventTrigger.triggers.Add(releaseButtonEntry);
+	}
+
+	private void SetRightButton () {
+		EventTrigger eventTrigger = transform.Find("ButtonRight").GetComponent<EventTrigger> ();
+
+		EventTrigger.Entry pushButtonEntry = new EventTrigger.Entry();
+		pushButtonEntry.eventID = EventTriggerType.PointerDown;
+		pushButtonEntry.callback.AddListener((data) => { playerManager.PushRightButton(); });
+		eventTrigger.triggers.Add(pushButtonEntry);
+
+		EventTrigger.Entry releaseButtonEntry = new EventTrigger.Entry();
+		releaseButtonEntry.eventID = EventTriggerType.PointerUp;
+		releaseButtonEntry.callback.AddListener((data) => { playerManager.ReleaseMoveButton(); });
+		eventTrigger.triggers.Add(releaseButtonEntry);
+	}
+
+	private void SetJumpButton () {
+		EventTrigger eventTrigger = transform.Find("ButtonJump").GetComponent<EventTrigger> ();
+
+		EventTrigger.Entry pushButtonEntry = new EventTrigger.Entry();
+		pushButtonEntry.eventID = EventTriggerType.PointerDown;
+		pushButtonEntry.callback.AddListener((data) => { playerManager.PushJumpButton(); });
+		eventTrigger.triggers.Add(pushButtonEntry);
 	}
 }
