90d0c532d60060d3b005cd7ab6768f9fcefc8cc9
ken1flan <ken1flan@gmail.com>
2018-07-23 20:09:24 +0900

    MapManagerからTilemapの設定をする

--- Assets/Scripts/MapManager.cs
+++ Assets/Scripts/MapManager.cs
@@ -1,16 +1,38 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Tilemaps;
 
 public class MapManager : MonoBehaviour {
 
 	// Use this for initialization
 	void Start () {
-		Debug.Log("Hello, MapManager");
+		SetGroundTilemap();
+		SetKeepoutTilemap ();
+		SetTrapTilemap ();
 	}
 
 	// Update is called once per frame
 	void Update () {
 
+	}
+
+	private void SetGroundTilemap () {
+		var tilemap = GameObject.Find("GroundTilemap");
+		tilemap.layer = LayerMask.NameToLayer("Block");
+		tilemap.AddComponent<TilemapCollider2D> ();
+	}
+
+	private void SetKeepoutTilemap () {
+		var tilemap = GameObject.Find("KeepoutTilemap");
+		tilemap.layer = LayerMask.NameToLayer("Keepout");
+		tilemap.AddComponent<TilemapCollider2D> ();
+	}
+
+	private void SetTrapTilemap () {
+		var tilemap = GameObject.Find("TrapTilemap");
+		tilemap.tag = "Trap";
+		var collider = tilemap.AddComponent<TilemapCollider2D> ();
+		collider.isTrigger = true;
 	}
 }
